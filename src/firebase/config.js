9bda32bb00ae4ffc80822c06922d32fae94958d4
Julien Lavie <j.lavie@betclicgroup.com>
2022-08-28 14:55:48 +0200

    configuration variabilization

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,19 +3,20 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB42Hijbtz4ZsiNjVGjYsdE3k1KvMq2yDs",
-  authDomain: "wedding-a2056.firebaseapp.com",
-  projectId: "wedding-a2056",
-  storageBucket: "wedding-a2056.appspot.com",
-  messagingSenderId: "1006617582181",
-  appId: "1:1006617582181:web:165e73d574ad3f2127c48c",
-  measurementId: "G-B3SWYEYFTY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -25,5 +26,6 @@ const analytics = getAnalytics(app);
 const projectStorage = getStorage();
 const projectFirestore = getFirestore(app);
 const db = getFirestore(app);
+const auth = getAuth(app)
 
-export { projectStorage, projectFirestore, db };
+export { projectStorage, projectFirestore, db, auth };
